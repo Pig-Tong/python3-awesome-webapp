56308c0b31076daad18c7d1fa7334e027cd2039e
Pig-Tong <191318554@qq.com>
2018-11-28 00:06:19 +0800

    update spider_huawa

--- spider_huawa/huawa_spider.py
+++ spider_huawa/huawa_spider.py
@@ -1,10 +1,7 @@
 # _*_ coding:utf-8 _*_
-import json
 import requests
-from requests.exceptions import RequestException
 import re
-import time
-from spider_huawa.huawa_area import get_huawa_area
+from common.img_to_string import img_to_string
 
 
 def get_one_page(url):
@@ -15,33 +12,61 @@ def get_one_page(url):
 
 
 def main():
-    area_list = get_huawa_area()
-    print(area_list[0])
-    for area in area_list[0]:
-        print(area)
-
-    # url = 'http://www.huawa.com/store-24-382-2465-0-0-0-0-0-2.html'
-    # html = get_one_page(url)
-    # print(parse_one_page(html))
+    # area_list = get_huawa_area()
+    # for province in area_list[0]:
+    #     province_id = int(province[0])
+    #     province_name = province[1]
+    #     for city in area_list[province_id]:
+    #         city_id = int(city[0])
+    #         city_name = city[1]
+    #         county_array = area_list[city_id]
+    #         if county_array:
+    #             for county in county_array:
+    #                 county_id = int(county[0])
+    #                 county_name = county[1]
+    #                 print(province_name, city_name, county_name)
+    #         else:
+    #             print(province_name, city_name)
+    url = 'http://www.huawa.com/store-24-382-2465-0-0-0-0-0-1.html'
+    html = get_one_page(url)
+    parse_one_page(html)
+    url = 'http://www.huawa.com/index.php?c=store_list&a=map&store_id=422422'
+    html = get_one_page(url)
+    parse_one_point(html)
 
 
 def parse_one_page(html):
+    result_list = []
     pattern = re.compile(
         '<span class="diqu">\[(.*?)\].<b><a href="http://www.huawa.com/shop/(.*?)" target="_blank">(.*?)</a>'
-        '.*?600;">(.*?)</font>.*?花店地址：(.*?)</p>.*?xinyu.*?title="(.*?)"',
+        '.*?600;">(.*?)</font>.*?花店电话:<img src="(.*?)".*?花店地址：(.*?)</p>.*?xinyu.*?title="(.*?)"',
+        re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        result = {
+            "id": item[1],
+            "area": item[0],
+            "name": item[2],
+            "address": item[5],
+            "phone": img_to_string(item[4]),
+            "status": item[3],
+            "other": item[6]
+        }
+        print(result)
+        result_list.append(result)
+    return result_list
+
+
+# 解析坐标
+def parse_one_point(html):
+    pattern = re.compile(
+        '"y_axis":"(.*?)","x_axis":"(.*?)"',
         re.S)
     items = re.findall(pattern, html)
     print(items)
     for item in items:
-        print(item)
-        # yield {
-        #     'area': item[19:],
-        #     # 'image': item[1],
-        #     # 'title': item[2].strip(),
-        #     # 'actor': item[3].strip()[3:] if len(item[3]) > 3 else '',
-        #     # 'time': item[4].strip()[5:] if len(item[4]) > 5 else '',
-        #     # 'score': item[5].strip() + item[6].strip()
-        # }
+        print(item[0])
+        print(item[1])
 
 
 main()
