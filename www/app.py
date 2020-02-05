12de7a22cc5de5aee355271f526012c921877f89
朱桐 <zhut96@192.168.0.109>
2020-02-05 22:34:21 +0800

    update app and orm

--- www/app.py
+++ www/app.py
@@ -6,22 +6,27 @@ __author__ = 'Pig·Tong'
 async web application.
 '''
 
-import logging; logging.basicConfig(level=logging.INFO)
+import logging
 
-import asyncio, os, json, time
-from datetime import datetime
+logging.basicConfig(level=logging.INFO)
+
+import asyncio
 
 from aiohttp import web
 
-def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
 
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+
+
+@asyncio.coroutine
 async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
--- www/orm.py
+++ www/orm.py
@@ -8,12 +8,11 @@ def log(sql, args=()):
     logging.info('sql:%s' % sql)
 
 
-@asyncio.coroutine
-def create_pool(loop, **kw):
+async def create_pool(loop, **kw):
     logging.info("create database connection pool...")
     global __pool
-    __pool = yield from aiomysql.create_pool(
-        host=kw.get('host', '47.91.210.125'),
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', '39.100.23.3'),
         port=kw.get('port', 3306),
         user=kw['user'],
         password=kw['password'],
@@ -26,34 +25,43 @@ def create_pool(loop, **kw):
     )
 
 
-@asyncio.coroutine
-def select(sql, args, size=None):
+async def select(sql, args, size=None):
     log(sql, args)
     global __pool
-    with (yield from __pool) as conn:
-        cur = yield from conn.cursor(aiomysql.DictCursor)
-        yield from cur.execute(sql.replace('?', '%s'), args or ())
-        if size:
-            rs = yield from cur.fetchmany(size)
-        else:
-            rs = yield from cur.fetchall()
-        yield from cur.close()
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
         logging.info('rows returned: %s' % len(rs))
         return rs
 
 
-@asyncio.coroutine
-def execute(sql, args):
+async def execute(sql, args, autocommit=True):
     log(sql)
-    with(yield from __pool) as conn:
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
         try:
-            cur = yield from conn.cursor()
-            yield from cur.execute(sql.replace('?', '%s'), args)
-            affected = cur.rowcount
-            yield from cur.close()
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
         except BaseException as e:
+            if not autocommit:
+                await conn.rollBack()
             raise
         return affected
+
+
+def create_args_string(num):
+    l = []
+    for n in range(num):
+        l.append('?')
+    return ','.join(l)
 
 
 class Field(object):
@@ -68,50 +76,69 @@ class Field(object):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
 
-def create_args_string(num):
-    L = []
-    for n in range(num):
-        L.append('?')
-    return ', '.join(L)
+class StringField(Field):
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
 
 
 class ModelMetaclass(type):
-    def __new__(cls, name, bases, attrs):
+    def __new__(mcs, name, bases, attrs):
         # 排除Model类本身
-        if name == 'Modle':
-            return type.__new__(cls, name, bases, attrs)
+        if name == 'Model':
+            return type.__new__(mcs, name, bases, attrs)
         # 获取table名称
-        tableName = attrs.get('__table__', None) or name
-        logging.info('found model:%s (table:%s)' % (name, tableName))
+        table_name = attrs.get('__table__', None) or name
+        logging.info('found model:%s (table:%s)' % (name, table_name))
         # 获取所有的Field和主键名:
         mappings = dict()
         fields = []
-        primaryKey = None
+        primary_key = None
         for k, v in attrs.items():
             if isinstance(v, Field):
                 logging.info('found mapping : %s ==> %s' % (k, v))
                 mappings[k] = v
                 if v.primary_key:
                     # 找到主键
-                    if primaryKey:
+                    if primary_key:
                         raise RuntimeError('Duplicate primary key for field: %s' % k)
                 else:
                     fields.append(k)
-        if not primaryKey:
+        if not primary_key:
             raise RuntimeError('Primary key not found.')
         for k in mappings.keys():
             attrs.pop(k)
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
         attrs['__mapping__'] = mappings  # 保存属性和列的映射关系
-        attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primaryKey
+        attrs['__table__'] = table_name
+        attrs['__primary_key__'] = primary_key
         attrs['__fields__'] = fields
-        attrs['__select__'] = 'select `%s`,%s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__select__'] = 'select `%s`,%s from `%s`' % (primary_key, ', '.join(escaped_fields), table_name)
         attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
-        tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+            table_name, ', '.join(escaped_fields), primary_key, create_args_string(len(escaped_fields) + 1))
         attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
-            tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
-        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
-        return type.__new__(cls, name, bases, attrs)
-
-    _
+            table_name, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primary_key)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (table_name, primary_key)
+        return type.__new__(mcs, name, bases, attrs)
